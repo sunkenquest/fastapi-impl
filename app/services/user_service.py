14e3be9d65c4a5f1b2b6422e4b4bfe58b8a97ef8
sunkenquest <sunkenquest@gmail.com>
2024-12-02 10:58:13 +0800

    feat: add get user api

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,11 +1,24 @@
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 from app.db.models import User
 from app.schemas.user import UserCreate
 
 
 def create_user(db: Session, user: UserCreate):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this email already exists.",
+        )
+
     db_user = User(name=user.name, email=user.email)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def find_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
