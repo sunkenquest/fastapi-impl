90c781584e90b420f42dd3cf216652ba7f4b617f
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-12-02 11:00:22 +0800

    feat: add get user api (#1)

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.db.connection import get_db
 from app.schemas.user import UserCreate, UserResponse
-from app.services.user_service import create_user
+from app.services.user_service import create_user, find_user
 
 router = APIRouter()
 
@@ -12,3 +12,11 @@ router = APIRouter()
 def post_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = create_user(db, user)
     return db_user
+
+
+@router.get("/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = find_user(db, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
