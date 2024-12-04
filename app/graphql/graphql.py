090f18bf43d4346c9f747ee218aa041f0f5bb3b5
sunkenquest <sunkenquest@gmail.com>
2024-12-04 17:29:20 +0800

    feat: add path params in graphql

--- app/graphql/graphql.py
+++ app/graphql/graphql.py
@@ -1,8 +1,37 @@
 import strawberry
+from typing import List
+
+
+@strawberry.type
+class Details:
+    message: str
+    is_positive: bool
+
+    """
+
+query {
+  hello(num: 42, greeting: "Hi") {
+    message
+    isPositive
+  }
+}
+    """
 
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def hello(self) -> str:
-        return "Hello World"
+    def hello(self, num: int, greeting: str = "Hello") -> Details:
+        """
+        A complex hello field that customizes the greeting and analyzes positivity.
+
+        Args:
+        - num (int): A number to evaluate.
+        - greeting (str): Customizable greeting text.
+
+        Returns:
+        - Details: Includes a message and positivity analysis.
+        """
+        is_positive = num > 0
+        message = f"{greeting} World, {num} is {'positive' if is_positive else 'negative or zero'}!"
+        return Details(message=message, is_positive=is_positive)
