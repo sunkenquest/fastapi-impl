0a848ec518337d7efa7425da85b1b8927d6e2666
sunkenquest <sunkenquest@gmail.com>
2024-12-10 11:07:02 +0800

    chore: adjust filter color

--- frontend/src/components/Filter.vue
+++ frontend/src/components/Filter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative inline-block">
         <button @click="toggleDropdown" id="dropdownDefaultButton"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="dark:bg-gray-900 text-white hover:bg-white hover:text-black focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center inline-flex items-center"
             type="button">
             Filter
             <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -12,7 +12,7 @@
         </button>
 
         <div :class="{ block: isDropdownVisible, hidden: !isDropdownVisible }" id="dropdownDefaultCheckbox"
-            class="z-10 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 m-2">
+            class="z-10 w-48  divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-900 m-2">
             <ul class="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownCheckboxButton">
                 <li>
                     <div class="flex items-center">
@@ -25,7 +25,7 @@
                 </li>
                 <li>
                     <div class="flex items-center">
-                        <input id="checkbox-item-2" type="checkbox" value="" checked
+                        <input id="checkbox-item-2" type="checkbox" value=""
                             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
                         <label for="checkbox-item-2" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                             Checked state
