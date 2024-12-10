f50ef3e9d0b9207a7b846db7ce1365bd29801e0c
sunkenquest <sunkenquest@gmail.com>
2024-12-10 10:56:54 +0800

    feat: add filter frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import Banner from './components/Banner.vue';
 import NavBar from './components/NavBar.vue';
-
+import Filter from './components/Filter.vue'
 </script>
 
 <template>
   <NavBar />
   <Banner />
+  <div class="px-5">
+    <Filter />
+  </div>
 </template>
 
 <style scoped></style>
